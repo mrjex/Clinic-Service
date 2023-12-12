5a87bc48b805404291e1b8a05bb576f37150656f
mohamadk <mohamadk@student.chalmers.se>
2023-12-12 20:14:37 +0100

    #4 (Ongoing) Bug fixes to MQTT client

--- Clinic-Service/src/main/java/com/group20/dentanoid/MqttMain.java
+++ Clinic-Service/src/main/java/com/group20/dentanoid/MqttMain.java
@@ -1,6 +1,9 @@
 package com.group20.dentanoid;
 
 import java.util.HashMap;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
 import org.eclipse.paho.client.mqttv3.IMqttDeliveryToken;
 import org.eclipse.paho.client.mqttv3.MqttCallback;
 import org.eclipse.paho.client.mqttv3.MqttClient;
@@ -13,6 +16,8 @@ public class MqttMain {
     public static HashMap<String, MqttMain> subscriptionManagers; // Format: // <"subTopic", new MqttMain Object>
     private static final String broker = "tcp://broker.hivemq.com:1883";
     private static MqttClient client;
+
+    static MemoryPersistence persistence = new MemoryPersistence();
     // Topic requirement:
     // {string1}/{string2}
 
@@ -37,7 +42,7 @@ public class MqttMain {
 
     // Create new instances of MqttMain that are mapped to their respective topics
     public static void initializeMqttConnection() {
-        subscriptionManagers = new HashMap<String, MqttMain>();
+        subscriptionManagers = new HashMap<>();
 
         for (int i = 0; i < subscriptions.length; i++) {
             subscriptionManagers.put(subscriptions[i], new MqttMain());
@@ -49,32 +54,35 @@ public class MqttMain {
         // String clientId = MqttClient.generateClientId();
         // MemoryPersistence persistence = new MemoryPersistence();
 
-        try {
-            // MqttClient sampleClient = new MqttClient(broker, clientId, persistence);
-            // MqttConnectOptions connOpts = new MqttConnectOptions();
-            // connOpts.setCleanSession(true);
-            // System.out.println("Connecting to broker: " + broker);
-            // sampleClient.connect(connOpts);
-            // System.out.println("Connected");
-            // System.out.println("Publishing message: " + content);
-            MqttMessage message = new MqttMessage(content.getBytes());
-            message.setQos(qos);
-            getInstance().publish(topic, message);
-            System.out.println("Message published");
+        // MqttClient sampleClient = new MqttClient(broker, clientId, persistence);
+        // MqttConnectOptions connOpts = new MqttConnectOptions();
+        // connOpts.setCleanSession(true);
+        // System.out.println("Connecting to broker: " + broker);
+        // sampleClient.connect(connOpts);
+        // System.out.println("Connected");
+        // System.out.println("Publishing message: " + content);
+        MqttMessage message = new MqttMessage(content.getBytes());
+        message.setQos(qos);
+        ExecutorService executorService = Executors.newSingleThreadExecutor();
 
-            /*
-             * sampleClient.disconnect(); //
-             * System.out.println("Disconnected"); //
-             * System.exit(0); //
-             */
-        } catch (MqttException me) {
-            System.out.println("reason " + me.getReasonCode());
-            System.out.println("msg " + me.getMessage());
-            System.out.println("loc " + me.getLocalizedMessage());
-            System.out.println("cause " + me.getCause());
-            System.out.println("excep " + me);
-            me.printStackTrace();
-        }
+        executorService.execute(() -> {
+            // Code to be executed in the new thread
+            try {
+                getInstance().publish(topic, message);
+            } catch (MqttException e) {
+                throw new RuntimeException(e);
+            }
+
+        });
+
+        executorService.shutdown();
+        System.out.println("Message published");
+
+        /*
+         * sampleClient.disconnect(); //
+         * System.out.println("Disconnected"); //
+         * System.exit(0); //
+         */
     }
 
     public static MqttClient getInstance() throws MqttException {
@@ -82,21 +90,26 @@ public class MqttMain {
             String username = "Test";
             String password = "Test123";
             String clientid = MqttClient.generateClientId();
-            client = new MqttClient(broker, clientid, new MemoryPersistence());
+            client = new MqttClient(broker, clientid, persistence);
+
             // connect options
             MqttConnectOptions options = new MqttConnectOptions();
+            options.setCleanSession(true);
             options.setUserName(username);
             options.setPassword(password.toCharArray());
-            options.setConnectionTimeout(60);
-            options.setKeepAliveInterval(60);
+            // options.setConnectionTimeout(60);
+            // options.setKeepAliveInterval(60);
             // setup callback
             client.setCallback(new MqttCallback() {
 
                 public void connectionLost(Throwable cause) {
-                    System.out.println("connectionLost: " + cause.getMessage());
+                    cause.printStackTrace();
                 }
 
                 public void messageArrived(String topic, MqttMessage message) throws MqttException {
+
+                    if (message.isRetained())
+                        return;
                     System.out.println("topic: " + topic);
                     System.out.println("Qos: " + message.getQos());
                     System.out.println("message content: " + new String(message.getPayload()));
@@ -107,8 +120,8 @@ public class MqttMain {
                 public void deliveryComplete(IMqttDeliveryToken token) {
                     System.out.println("deliveryComplete---------" + token.isComplete());
                 }
-
             });
+
             client.connect(options);
         }
         return client;
