69d02b6752b39b8dabd3323c17b26dac65eaf035
joelmat <joelmat@student.chalmers.se>
2023-12-19 22:16:18 +0100

    #5 (on-going) Distinguish between the case where a clinic is found and when its not found

--- Clinic-Service/src/main/java/com/group20/dentanoid/GoogleAPI/public/apiQuery.js
+++ Clinic-Service/src/main/java/com/group20/dentanoid/GoogleAPI/public/apiQuery.js
@@ -1,49 +1,52 @@
 let map;
 let service;
+let clinicNameRequest;
 
 function initMap() {
   // Keep in mind: Reading from validatedClinic.json has to happen after its content is updated from DentalClinic.java
   fetch('./validatedClinic.json')
   .then(response => response.json())
   .then(clinicRequestData => {
-    console.warn(clinicRequestData)
-  
-    const payloadCoordinates = clinicRequestData.position.split(',')
-  
-  /*
-   TODO:
-   2) Read name of clinic and check it in 'callback()'
-   3) Research how to write to JSON text file in a similar way as the technique above, where 'require fs module' isn't needed
-  */
-
-   const clinicCoordinates = new google.maps.LatLng(payloadCoordinates[0], payloadCoordinates[1]);
-
-   // Generate the back-end map to access global coordinates of its nodes (dental clinics)
-   map = new google.maps.Map(document.getElementById('map'), {
-       center: clinicCoordinates
-   });
- 
-   var request = {
-     location: clinicCoordinates,
-     radius: '300',
-     type: ['dentist']
-   };
- 
-   service = new google.maps.places.PlacesService(map);
-   service.nearbySearch(request, callback);
-
+    simulateMap(clinicRequestData)
   })
   .catch(error => console.error('Error fetching JSON:', error));
+}
+
+// Generate the back-end map dependencies to access global coordinates of its nodes (dental clinics)
+function simulateMap(clinicRequestData) {
+/*
+ TODO:
+ 1) Research how to write to JSON text file in a similar way as the technique above, where 'require fs module' isn't needed
+*/
+  
+clinicNameRequest = clinicRequestData.clinic_name
+  const payloadCoordinates = clinicRequestData.position.split(',')
+
+  const clinicCoordinates = new google.maps.LatLng(payloadCoordinates[0], payloadCoordinates[1]);
+
+ // Generate the back-end map to access global coordinates of its nodes (dental clinics)
+ map = new google.maps.Map(document.getElementById('map'), {
+     center: clinicCoordinates
+ });
+
+ var request = {
+   location: clinicCoordinates,
+   radius: '300',
+   type: ['dentist']
+ };
+
+ service = new google.maps.places.PlacesService(map);
+ service.nearbySearch(request, callback);
 }
 
 function callback(results, status) {
   if (status == google.maps.places.PlacesServiceStatus.OK) {
     for (var i = 0; i < results.length; i++) {
+      const potentialClinic = results[i].name
 
-      console.warn(results[i])
-
-      // TODO: Check if 'results[i].name' === payload name
-      console.warn(results[i].name)
+      if (potentialClinic === clinicNameRequest) {
+        console.warn('CLINIC VALIDATED!')
+      }
     }
   }
 }
